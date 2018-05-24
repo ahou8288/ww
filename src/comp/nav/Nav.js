5e71ff6b669c20e36d572769c012ff8c11503c3a
Andrew Houghton <houghtonandrew0@gmail.com>
2018-05-25 08:00:24 +1000

    Changed to bottom navigation. Routing is mostly done in Nav

--- src/comp/nav/Nav.js
+++ src/comp/nav/Nav.js
@@ -1,31 +1,52 @@
-import React, { Component } from 'react'
+import React from 'react'
 import 'semantic-ui-css/semantic.min.css';
 import './nav.css';
-import { Sidebar, Segment, Button, Menu, Icon, Item } from 'semantic-ui-react'
+import { Menu, Icon } from 'semantic-ui-react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+import headerLogo from '../../img/logo.svg';
 
 // My components
-import App from '../app/App'
-import Sidelinks from '../sidelinks/Sidelinks'
+import LevelIndex from '../levels/LevelIndex'
+import GuideIndex from '../guides/GuideIndex'
+import OverviewMap from '../map/OverviewMap'
 
 const Nav = () => (
-  <div class="nav">
-    <App/>
-    <Menu attached='bottom' tabular>
-      <Menu.Item>
-      <Icon name='camera' />
-        Levels
-      </Menu.Item>
+  <div>
+    <Router>
+      <div>
+        <Route exact path="/" component={LevelIndex}/>
+        <Route path="/levels" component={LevelIndex}/>
+        <Route path="/guides" component={GuideIndex}/>
+        <Route path="/map" component={OverviewMap}/>
+    
+        <Menu id="bottomnav" inverted>
+          <Menu.Item>
+            <img src={headerLogo} className="App-logo" alt="logo" />
+          </Menu.Item>
 
-      <Menu.Item>
-      <Icon name='camera' />
-        Guides
-      </Menu.Item>
+          <Link to="/levels">
+            <Menu.Item>
+            <Icon name='chart bar' size="large"/>
+              Levels
+            </Menu.Item>
+          </Link>
 
-      <Menu.Item>
-      <Icon name='camera' />
-        Map
-      </Menu.Item>
-    </Menu>
+          <Link to="/map">
+            <Menu.Item>
+              <Icon name='info circle' size="large"/>
+              Guides
+            </Menu.Item>
+          </Link>
+          <Link to="/guides">
+            <Menu.Item>
+              <Icon name='map marker alternate' size="large"/>
+              Map
+            </Menu.Item>
+          </Link>
+        </Menu>
+      </div>
+    </Router>
   </div>
 )
 
